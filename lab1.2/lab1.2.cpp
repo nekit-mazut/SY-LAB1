b3bd12da24d6c21070b3acbb37147ccd7eaee694
nekit-mazut <145917921+nekit-mazut@users.noreply.github.com>
2026-03-18 22:16:10 +0500

    Add files via upload

---
+++ lab1.2/lab1.2.cpp
@@ -0,0 +1,26 @@
+﻿#include <stdio.h>
+
+// Макрос для вывода
+#define pr(x) printf("x = %u,  *x = %d,  &x = %u\n", (unsigned)x, *x, (unsigned)&x)
+
+int main(void) {
+    int mas[] = { 100, 200, 300 };
+
+    int* ptr1;
+    int* ptr2;
+
+    ptr1 = mas;
+    ptr2 = &mas[2];
+
+    pr(ptr1);
+    ptr1++;                          // сдвигаем указатель на следующий элемент (mas[1])
+    pr(ptr1);
+
+    pr(ptr2);
+
+    ++ptr2;                          // сдвигаем ptr2 за пределы массива
+
+    printf("ptr2 - ptr1 = %u\n", (unsigned)(ptr2 - ptr1));
+
+    return 0;
+}
